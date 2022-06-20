60b3e2d5b9e1cdac1f747e08a410ed63e5b7a6ce
unknown <shivamkumar0170951@gmail.com>
2022-06-20 17:21:24 +0530

    shivam

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App(){
+    return(
+        <>
+            <h1 className="outbox">Weather App 🌦 ☔ ☼ ☀ ☁</h1>
+        </>
+    )
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import  App from './App';
+import Tempapp from "./components/Tempapp";
+import './App.css'
+
+ReactDOM.render(
+    <>
+        <App />
+        <Tempapp />
+    </>,
+    document.getElementById('root'));
